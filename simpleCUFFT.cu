eae1ba44c0194e70d15e8033694e7eddda15cc1c
Sarma <vtangira@buffalo.edu>
2013-03-31 23:43:16 -0400

    Code Cleanup with Functions.

--- simpleCUFFT.cu
+++ simpleCUFFT.cu
@@ -27,8 +27,11 @@
 
 typedef float2 Complex;
 
-void printData(Complex *a, int size)
-{
+void printData(Complex *a, int size, char *msg) {
+
+  if (msg == "") printf("\n");
+  else printf("%s\n", msg);
+
   for (int i = 0; i < size; i++)
     printf("%f %f\n", a[i].x, a[i].y);
 }
@@ -41,56 +44,81 @@ void normData(Complex *a, int size, float norm) {
   }
 }
 
+// flag = 1 for real signals.
+void randomFill(Complex *h_signal, int size, int flag) {
+
+  // Real signal.
+  if (flag == 1) {
+    for (int i = 0; i < size; i++) {
+      h_signal[i].x = rand() / (float) RAND_MAX;
+      h_signal[i].y = 0;
+    }
+  }
+}
+
+// FFT a signal that's on the _DEVICE_.
+void signalFFT(Complex *d_signal, int signal_size) {
+
+  // Handle type used to store and execute CUFFT plans.
+  // Essentially allocates the resouecwes and sort of interns
+  // them.
+
+  cufftHandle plan;
+  cufftPlan1d(&plan, signal_size, CUFFT_C2C, 1);
+  printf("Plan Created\n");
+
+  // Execute the plan.
+  cufftExecC2C(plan, (cufftComplex *) d_signal, (cufftComplex *) d_signal, CUFFT_FORWARD);
+}
+
+void signalIFFT(Complex *d_signal, int signal_size) {
+
+  // Reverse of the signalFFT(.) function.
+
+  cufftHandle plan;
+  cufftPlan1d(&plan, signal_size, CUFFT_C2C, 1);
+  printf("Plan Created\n");
+
+  cufftExecC2C(plan, (cufftComplex *) d_signal, (cufftComplex *) d_signal, CUFFT_INVERSE);
+}
+
 int main()
 {
 
-  Complex *h_signal, *d_signal;
+  Complex *h_signal, *d_signal1, *d_signal2;
 
   int alloc_size = SIGNAL_SIZE;
 
   h_signal = (Complex *) malloc(sizeof(Complex) * alloc_size);
 
-  //checkCudaErrors(cudaMalloc(&d_signal, sizeof(Complex) * alloc_size));
-  cudaMalloc(&d_signal, sizeof(Complex) * alloc_size);
+  cudaMalloc(&d_signal1, sizeof(Complex) * alloc_size);
+  cudaMalloc(&d_signal2, sizeof(Complex) * alloc_size);
 
-  for (int i = 0; i < SIGNAL_SIZE; i++) {
-    h_signal[i].x = rand() / (float) RAND_MAX;
-    //h_signal[i].y = rand() / (float) RAND_MAX;
-    h_signal[i].y = 0;
-  }
+  // Add random data to signal.
+  randomFill(h_signal, alloc_size, 1);
+  printData(h_signal, alloc_size, "H1");
+  cudaMemcpy(d_signal1, h_signal, sizeof(Complex) * alloc_size, cudaMemcpyHostToDevice);
 
-  printData(h_signal, alloc_size);
-
-  //checkCudaErrors(cudaMemcpy(d_signal, h_signal, sizeof(Complex) * alloc_size, cudaMemcpyHostToDevice));
-  cudaMemcpy(d_signal, h_signal, sizeof(Complex) * alloc_size, cudaMemcpyHostToDevice);
+  randomFill(h_signal, alloc_size, 1);
+  printData(h_signal, alloc_size, "H2");
+  cudaMemcpy(d_signal2, h_signal, sizeof(Complex) * alloc_size, cudaMemcpyHostToDevice);
 
   printf("MemCpy Fwd\n");
 
-  // Handle type used to store and execute CUFFT plans.
-  cufftHandle plan;
-  //checkCudaErrors(cufftPlan1d(&plan, alloc_size, CUFFT_C2C, 1));
-  cufftPlan1d(&plan, alloc_size, CUFFT_C2C, 1);
+  signalFFT(d_signal1, alloc_size);
+  signalIFFT(d_signal1, alloc_size);
+  signalFFT(d_signal2, alloc_size);
+  signalIFFT(d_signal2, alloc_size);
 
-  //getLastCudaError("Kernel execution failed [ ComplexPointwiseMulAndScale ]");
+  printf("FFT Executed\n");
 
-  printf("Plan Created\n");
-
-  //checkCudaErrors(cufftExecC2C(plan, (cufftComplex *) d_signal, (cufftComplex *) d_signal, CUFFT_FORWARD));
-  cufftExecC2C(plan, (cufftComplex *) d_signal, (cufftComplex *) d_signal, CUFFT_FORWARD);
-
-  //checkCudaErrors(cufftExecC2C(plan, (cufftComplex *) d_signal, (cufftComplex *) d_signal, CUFFT_INVERSE));
-  cufftExecC2C(plan, (cufftComplex *) d_signal, (cufftComplex *) d_signal, CUFFT_INVERSE);
-
-  printf("Executed\n");
-
-  //checkCudaErrors(cudaMemcpy(h_signal, d_signal, sizeof(Complex) * alloc_size, cudaMemcpyDeviceToHost));
-  cudaMemcpy(h_signal, d_signal, sizeof(Complex) * alloc_size, cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_signal, d_signal2, sizeof(Complex) * alloc_size, cudaMemcpyDeviceToHost);
 
   printf("MemCpy Bakwd\n");
 
   normData(h_signal, alloc_size, 10);
 
-  printData(h_signal, alloc_size);
+  printData(h_signal, alloc_size, "D2 after FFT.");
 
   return 0;
 }
